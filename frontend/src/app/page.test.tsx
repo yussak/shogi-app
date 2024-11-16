83a66f55873d2124b9bfc9007a15ab42f3bdd0e1
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-11-17 01:27:02 +0900

    テスト追加

---
+++ frontend/src/app/page.test.tsx
@@ -0,0 +1,54 @@
+import { render, fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { describe, expect, it } from "vitest";
+import Home from "./page";
+
+describe("先手", () => {
+  it("盤上の歩が1マス上にさせる", async () => {
+    render(<Home />);
+
+    // 初期位置に駒があることを確認
+    const initialPiece = screen.getByTestId("piece-6-0");
+
+    // セルの存在を確認
+    const targetCell = screen.getByTestId("cell-5-0");
+
+    // 初期位置のセルをクリック
+    fireEvent.click(initialPiece);
+
+    // 移動先のセルをクリック
+    fireEvent.click(targetCell);
+
+    // 駒が移動したことを確認
+    expect(screen.queryByTestId("piece-6-0")).not.toBeInTheDocument();
+    expect(screen.getByTestId("piece-5-0")).toBeInTheDocument();
+  });
+
+  //   it("盤上の歩がさせる位置が正しい", () => {
+  //     //
+  //   });
+
+  //   it("駒を取得したら盤上に追加される", () => {
+  //     //
+  //   });
+
+  //   it("駒台の歩が打てる位置が正しい", () => {
+  //     //
+  //   });
+
+  //   it("駒台から歩を打つ時に二歩できない", () => {
+  //     //
+  //   });
+
+  //   it("3マスより上にいる時に成れる", () => {
+  //     //
+  //   });
+
+  //   it("成らないことも可能", () => {
+  //     //
+  //   });
+
+  //   it("1マス目に指したら自動で成る", () => {
+  //     //
+  //   });
+});
