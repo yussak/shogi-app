c423f3299442405404639f041068a2ea750d112b
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-11-17 01:26:57 +0900

    コンポーネント改善

--- frontend/src/components/ui/Board.tsx
+++ frontend/src/components/ui/Board.tsx
@@ -1,45 +1,34 @@
-import { OPPONENT } from "@/consts";
-import { Piece } from "@/types";
+import Cell from "./Cell";
 
-type Props = {
-  rowIndex: number;
-  colIndex: number;
-  piece: Piece;
-  selectedPiece: Piece;
-  isavailablePosition: boolean;
-  handleCellClick: (piecerowIndex: number, colIndex: number) => void;
-};
-
-const Board = ({ rowIndex, colIndex, piece, isavailablePosition, selectedPiece, handleCellClick }: Props) => {
+const Board = ({ rows, columns, pieces, selectedPiece, handleCellClick, getavailablePositions }) => {
   return (
     <div
-      key={`${rowIndex}-${colIndex}`}
-      onClick={() => handleCellClick(rowIndex, colIndex)}
       style={{
-        width: "70px",
-        height: "70px",
-        border: "1px solid black",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-
-        backgroundColor: isavailablePosition
-          ? "#A3D2CA" // 移動可能位置の色
-          : selectedPiece &&
-            selectedPiece.position &&
-            selectedPiece.position[0] === rowIndex &&
-            selectedPiece.position[1] === colIndex
-          ? "#FFD700" // 選択中の駒の色
-          : "#F0D9B5",
+        display: "grid",
+        gridTemplateColumns: "repeat(9, 70px)",
       }}
     >
-      {piece && (
-        <div
-          // 相手の駒の場合逆さまにする
-          style={{ transform: piece.owner === OPPONENT ? "rotate(180deg)" : "none" }}
-        >
-          {piece && piece.type === "fuhyou" ? (piece.isPromoted ? "と" : "歩") : ""}
-        </div>
+      {rows.map((_, rowIndex) =>
+        columns.map((_, colIndex) => {
+          // マスに駒があるかを確認
+          const piece = pieces.find((p) => p.position[0] === rowIndex && p.position[1] === colIndex);
+
+          const movablePositions = selectedPiece ? getavailablePositions(selectedPiece) : [];
+          const isavailablePosition = movablePositions.some(
+            (position) => position[0] === rowIndex && position[1] === colIndex
+          );
+
+          return (
+            <Cell
+              rowIndex={rowIndex}
+              colIndex={colIndex}
+              piece={piece}
+              isavailablePosition={isavailablePosition}
+              selectedPiece={selectedPiece}
+              handleCellClick={handleCellClick}
+            />
+          );
+        })
       )}
     </div>
   );
---
+++ frontend/src/components/ui/Cell.tsx
@@ -0,0 +1,50 @@
+import { OPPONENT } from "@/consts";
+import { Piece } from "@/types";
+
+type Props = {
+  rowIndex: number;
+  colIndex: number;
+  piece: Piece;
+  selectedPiece: Piece;
+  isavailablePosition: boolean;
+  handleCellClick: (piecerowIndex: number, colIndex: number) => void;
+};
+
+const Cell = ({ rowIndex, colIndex, piece, isavailablePosition, selectedPiece, handleCellClick }: Props) => {
+  return (
+    <div
+      key={`${rowIndex}-${colIndex}`}
+      data-testid={`cell-${rowIndex}-${colIndex}`}
+      onClick={() => handleCellClick(rowIndex, colIndex)}
+      style={{
+        width: "70px",
+        height: "70px",
+        border: "1px solid black",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+
+        backgroundColor: isavailablePosition
+          ? "#A3D2CA" // 移動可能位置の色
+          : selectedPiece &&
+            selectedPiece.position &&
+            selectedPiece.position[0] === rowIndex &&
+            selectedPiece.position[1] === colIndex
+          ? "#FFD700" // 選択中の駒の色
+          : "#F0D9B5",
+      }}
+    >
+      {piece && (
+        <div
+          data-testid={`piece-${rowIndex}-${colIndex}`}
+          // 相手の駒の場合逆さまにする
+          style={{ transform: piece.owner === OPPONENT ? "rotate(180deg)" : "none" }}
+        >
+          {piece && piece.type === "fuhyou" ? (piece.isPromoted ? "と" : "歩") : ""}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Cell;
