8bd7475560cce98b52b7d3c1b54ae1f57cdd8ef2
yussak <yusuke.sakuraba0213@gmail.com>
2024-12-20 21:52:29 +0900

    ビルドエラー対処

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,8 +7,12 @@ import { useState } from "react";
 import Board from "@/components/ui/Board";
 import { initialPieces, isPromotionZone } from "@/utils";
 
+type HomeProps = {
+  initialPiecesOverride?: Piece[];
+}
+
 // TODO:指した手をどこかに表示したい。先手24歩　後手84歩とか
-export default function Home({ initialPiecesOverride }: { initialPiecesOverride?: Piece[] }) {
+export default function Home({ initialPiecesOverride }: HomeProps) {
   const [pieces, setPieces] = useState<Piece[]>(initialPiecesOverride || initialPieces);
   const [selectedPiece, setSelectedPiece] = useState<Piece | null>(null);
   const [capturedPieces, setCapturedPieces] = useState<CapturedPiece[]>([]);
